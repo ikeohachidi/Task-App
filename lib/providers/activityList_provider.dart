b374c020c0b2a27fcc69226f6c938a1fec770725
CodeBrown <ikeohachidi@gmail.com>
2019-11-02 21:57:39 +0100

    created provider class

--- lib/providers/activityList_provider.dart
+++ lib/providers/activityList_provider.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 
+class DateActivity {
+  final String day;
+  final String date;
+  final String month;
+
+  DateActivity({this.day, this.date, this.month});
+}
 
 // Gets the date needed by the user
 class ActivityListProvider with ChangeNotifier {
-  String _chosenDate;
 
-  ActivityListProvider(this._chosenDate);
+  DateActivity chosenDate;
 
-  get getDate => this._chosenDate;
+  ActivityListProvider() {
+    this.chosenDate = DateActivity(day: "", date: "", month: "");
+  }
 
-  set setChosenDate(String chosenDate) {
-    this._chosenDate = chosenDate;
+  get getDate => this.chosenDate;
+
+  get builtDate => "${this.chosenDate.month} ${this.chosenDate.date} ${this.chosenDate.day}";
+
+  set setChosenDate(DateActivity date) {
+    this.chosenDate = date;
     notifyListeners();
   }
 
